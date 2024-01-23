2888f76249a315266de00e4de73621ed4ffa75ae
bilal1718 <muhbil171845@gmail.com>
2024-01-23 11:00:16 +0500

    Navbar Scroll Y Background changing

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 
 const Nav = () => {
+  const [navbar,setNavBar]=useState(false);
+  const changeBackground=()=>{
+    if(window.scrollY >=120){
+      setNavBar(true);
+    }else{
+      setNavBar(false);
+    }
+  }
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener('scroll', changeBackground);
+  }, []);
   return (
-    <div>
-         <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <header>
+        <nav className={`bg-${navbar ? 'black' : 'white'} text-${navbar ? 'black' : 'white'} border-gray-200 dark:bg-gray-900`}>
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a
       href=""
@@ -120,12 +132,11 @@ const Nav = () => {
           placeholder="Search..."
         />
       </div>
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className={`flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg ${navbar ? 'text-white' : 'text-black'} md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700`}>
         <li>
           <a
             href="#"
-            className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-            aria-current="page"
+            className={`${navbar ? 'decoration-white' : 'text-black'} block py-2 px-3  rounded md:bg-transparent md:text-black-700 md:p-0 md:dark:text-black-500`}
           >
             Home
           </a>
@@ -133,7 +144,7 @@ const Nav = () => {
         <li>
           <a
             href="#"
-            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            className={`${navbar ? 'decoration-white' : 'text-black'} block py-2 px-3  rounded md:bg-transparent md:text-black-700 md:p-0 md:dark:text-black-500`}
           >
             TV Shows
           </a>
@@ -141,7 +152,7 @@ const Nav = () => {
         <li>
           <a
             href="#"
-            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            className={`${navbar ? 'decoration-white' : 'text-black'} block py-2 px-3  rounded md:bg-transparent md:text-black-700 md:p-0 md:dark:text-black-500`}
           >
             Movies
           </a>
@@ -149,7 +160,7 @@ const Nav = () => {
         <li>
           <a
             href="#"
-            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            className={`${navbar ? 'decoration-white' : 'text-black'} block py-2 px-3  rounded md:bg-transparent md:text-black-700 md:p-0 md:dark:text-black-500`}
           >
             Latest
           </a>
@@ -157,7 +168,7 @@ const Nav = () => {
         <li>
           <a
             href="#"
-            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            className={`${navbar ? 'decoration-white' : 'text-black'} block py-2 px-3  rounded md:bg-transparent md:text-black-700 md:p-0 md:dark:text-black-500`}
           >
             My List
           </a>
@@ -166,7 +177,7 @@ const Nav = () => {
     </div>
   </div>
 </nav>
-    </div>
+    </header>
   )
 }
 
