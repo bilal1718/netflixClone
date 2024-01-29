6d652c1257f1eef6c130cdaa912218413c84c876
bilal1718 <muhbil171845@gmail.com>
2024-01-29 11:30:03 +0500

    Thr component for saving favorite movies

---
+++ components/MyList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+
+const MyList = () => {
+  return (
+    <div className='bg-zinc-900'>
+    <Nav />
+    </div>
+  )
+}
+
+export default MyList
