f5ad6219e660de6d8f94e0ab4a1ed86d677a8a14
bilal1718 <muhbil171845@gmail.com>
2024-01-30 11:21:30 +0500

    The additional styling and addLogic list

--- App.jsx
+++ App.jsx
@@ -5,11 +5,23 @@ import Home from "./components/Home";
 import MyList from "./components/MyList";
 
 export default function App() {
+  const [listItems,setListItems]=useState([]);
+  const AddtoList=(movie)=>{
+    const itemExist=listItems.find((item)=>item.movie.id===movie.id);
+    if(itemExist){
+      const updateList=listItems.map((list)=>(
+        list.id===movie.id ? {...list, quantity:quantity + 1} : list
+      ))
+      setListItems(updateList);
+    }else{
+      setListItems([...listItems,{movie:movie,quantity:1}]);
+    }
+  }
   return (
     <BrowserRouter>
     <Routes>
-  <Route index element={<Home />} />
-  <Route path="myList" element={<MyList />}/>
+  <Route index element={<Home AddtoList={AddtoList} />} />
+  <Route path="myList" element={<MyList listItems={listItems} />}/>
       </Routes>
         </BrowserRouter>
         )}
