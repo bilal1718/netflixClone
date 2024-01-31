edb960eef41c3a8b83bcf5b4b2d6541bcf2330ac
bilal1718 <muhbil171845@gmail.com>
2024-01-31 17:55:54 +0500

    The props to various elements

--- App.jsx
+++ App.jsx
@@ -8,6 +8,7 @@ import Full_Movies from "./components/Full_Movies";
 
 export default function App() {
   const [listItems,setListItems]=useState([]);
+  const [filteredData,setFilteredData]=useState([]);
   const AddtoList=(movie)=>{
     const itemExist=listItems.find((item)=>item.movie.id===movie.id);
     if(itemExist){
@@ -15,15 +16,30 @@ export default function App() {
         list.id===movie.id ? {...list, quantity:quantity + 1} : list
       ))
       setListItems(updateList);
+      setFilteredData(updateList);
     }else{
       setListItems([...listItems,{movie:movie,quantity:1}]);
+      setFilteredData([...listItems,{movie:movie,quantity:1}])
     }
   }
+  const FilterByMovieName = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    setFilteredData(
+      listItems.filter((item) => {
+        const movieTitle =item.movie.title || item.movie.name;
+        return movieTitle.toLowerCase().includes(searchTerm);
+      })
+    );
+  };
   return (
     <BrowserRouter>
     <Routes>
   <Route index element={<Home AddtoList={AddtoList} />} />
-  <Route path="myList" element={<MyList listItems={listItems} />}/>
+  <Route path="myList"
+   element={
+  <MyList listItems={listItems}
+  filteredData={filteredData}
+  FilterByMovieName={FilterByMovieName} />}/>
   <Route path="tvShows" element={<Full_TV_Show AddtoList={AddtoList} />} />
   <Route path="allMovies" element={<Full_Movies AddtoList={AddtoList} />} />
       </Routes>
