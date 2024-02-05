bbe7c98bcd8415514b75ae555a577eb237d25857
bilal1718 <muhbil171845@gmail.com>
2024-02-05 10:46:49 +0500

    implement logic of back to top button

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
-import Nav from "./Nav";
-import Hero from "./Hero";
-import "../App.css";
-import Movies from "./Movies";
-import Footer from "./footer";
+import React, { useEffect, useState } from 'react';
+import Nav from './Nav';
+import Hero from './Hero';
+import '../App.css';
+import Movies from './Movies';
+import Footer from './footer';
 
-const Home = ({AddtoList}) => {
+const Home = ({ AddtoList }) => {
+  const [showBackToTop, setShowBackToTop] = useState(false);
+
+  useEffect(() => {
+    function scrollFunction() {
+      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        setShowBackToTop(true);
+      } else {
+        setShowBackToTop(false);
+      }
+    }
+    window.addEventListener('scroll', scrollFunction);
+  }, []);
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
   return (
     <div className='scroll-container'>
+      {showBackToTop && (
+        <button className='back-to-top' onClick={backToTop}>
+          Back to top
+        </button>
+      )}
       <Nav />
       <Hero />
       <Movies AddtoList={AddtoList} />
       <Footer />
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
